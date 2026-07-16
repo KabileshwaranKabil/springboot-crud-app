fb34785cc205b40dfc4481263cb356326e475b63
Kabileshwarankabil <kabileshwaran1896@gmail.com>
2026-07-16 17:42:06 +0530

    spring security, jwt tokens

--- src/main/java/com/example/demo_spring/controller/AuthController.java
+++ src/main/java/com/example/demo_spring/controller/AuthController.java
@@ -1,5 +1,15 @@
 package com.example.demo_spring.controller;
 
+import com.example.demo_spring.model.User;
+import com.example.demo_spring.repository.UserRepository;
+import com.example.demo_spring.service.UserService;
+import com.example.demo_spring.util.JwtUtil;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,13 +17,38 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 public class AuthController {
+    private final UserService userService;
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
     @PostMapping("/login")
-    public String loginUser(@RequestBody Map<String,String> body){
+    public ResponseEntity<?> loginUser(@RequestBody Map<String,String> body){
+        String email = body.get("email");
+        String password = body.get("password");
+
+        var user = userRepository.findByEmail(email);
+        if(user.isEmpty()){
+           return new ResponseEntity<>("User is not registered",HttpStatus.UNAUTHORIZED);
+        }
+        User userd = user.get();
+        if(!passwordEncoder.matches(password,userd.getPassword())){
+            return new ResponseEntity<>("Invalid user",HttpStatus.UNAUTHORIZED);
+        }
+        String token = jwtUtil.generateToken(email);
+        return ResponseEntity.ok(Map.of("token",token));
 
     }
     @PostMapping("/register")
-    public String registerUser(@RequestBody Map<String,String> body){
+    public ResponseEntity<String> registerUser(@RequestBody Map<String,String> body){
+        String email = body.get("email");
+        String password = body.get("password");
 
+        if(userRepository.findByEmail(email).isPresent()){
+            return new ResponseEntity<>("Email alreday taken", HttpStatus.CONFLICT);
+        }
+        userService.createUser(User.builder().email(email).password(password).build());
+        return new ResponseEntity<>("Successfully registered",HttpStatus.CREATED);
     }
 }
